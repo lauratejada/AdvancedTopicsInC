a64bca29bf775d5132c4b52f5fe53687740b162b
LauraTejada <laura2191@hotmail.com>
2023-09-20 23:40:20 -0500

    First commit lab2

---
+++ IntroToUsers-Lab5/Areas/Identity/Data/DemoUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using IntroToUsers_Lab5.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IntroToUsers_Lab5.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the DemoUser class
+public class DemoUser : IdentityUser
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    // Navigation property for user's accounts
+    public HashSet<Account> Accounts { get; set; } = new HashSet<Account>();
+}
+
---
+++ IntroToUsers-Lab5/Models/ViewModels/CreateAccountVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroToUsers_Lab5.Models.ViewModels
+{
+    public class CreateAccountVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Starting deposit must be greater than zero.")]
+        public decimal StartingDeposit { get; set; }
+    }
+}
